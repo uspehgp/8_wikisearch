554729e9f6fd2b63815f98514bd322a4ecd946af
uspehgp <uspehgp@gmail.com>
2021-09-19 12:17:01 +0300

    create table of pages at page list component

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -7,7 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class PageListComponent implements OnInit {
 
-  @Input() pages = [];
+  @Input() pages: any = [];
 
   constructor() {
   }
