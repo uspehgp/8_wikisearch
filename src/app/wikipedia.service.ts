f3e6556a112ddc675d9608def49b213d6fb801bf
uspehgp <uspehgp@gmail.com>
2021-09-19 10:03:11 +0300

    receiving response from wiki at console

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
-  search(term: string){
-    return 'I am wiki search result'
+  search(term: string) {
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*'
+      }
+    })
   }
 }
